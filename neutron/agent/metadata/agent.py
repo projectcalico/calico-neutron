dbf50a011d34d454f4aadcb3ad3561b31e515533
Cory Benfield <cory.benfield@metaswitch.com>
2014-07-10 15:02:53 +0100

    Accept flat networks in the Neutron metadata agent
    The Neutron metadata agent implicitly expects nested networks
    by requiring that it have either a router ID or a network ID. In
    flat networks (like the one set up by Calico), one metadata agent
    may serve ports on multiple networks and without OpenStack
    routers.
    
    This patch allows the metadata agent (and the namespace proxy) to
    function in a network that does not use virtual layer 2 networks,
    by relying on the fact that in such an OpenStack deployment IP
    addresses will be unique across the data center.

--- neutron/agent/metadata/agent.py
+++ neutron/agent/metadata/agent.py
@@ -145,12 +145,16 @@ class MetadataProxyHandler(object):
 
         """
         qclient = self._get_neutron_client()
-
         all_ports = qclient.list_ports(
-            network_id=networks,
             fixed_ips=['ip_address=%s' % remote_address])['ports']
+
         self.auth_info = qclient.get_auth_info()
-        return all_ports
+        if networks:
+            networks = set(networks)
+            return [p for p in all_ports if p['network_id'] in networks]
+        else:
+            # Flat networks should have unique IPs.
+            return all_ports
 
     def _get_ports(self, remote_address, network_id=None, router_id=None):
         """Search for all ports that contain passed ip address and belongs to
@@ -165,8 +169,7 @@ class MetadataProxyHandler(object):
         elif router_id:
             networks = self._get_router_networks(router_id)
         else:
-            raise TypeError(_("Either one of parameter network_id or router_id"
-                              " must be passed to _get_ports method."))
+            networks = None
 
         return self._get_ports_for_remote_address(remote_address, networks)
 
